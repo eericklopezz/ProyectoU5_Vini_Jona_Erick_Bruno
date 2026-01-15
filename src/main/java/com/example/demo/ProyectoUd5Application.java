6533cb3fc0c7c1e4120435f183aeca9956bdfec2
Vincenzo <Vincenzo@10.1.200.128>
2026-01-15 09:18:46 +0100

    Commit proyecto

---
+++ src/main/java/com/example/demo/ProyectoUd5Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoUd5Application {
+
+	public static void main(String[] args) {
+		//Proyecto UD5
+		SpringApplication.run(ProyectoUd5Application.class, args);
+	}
+
+}
