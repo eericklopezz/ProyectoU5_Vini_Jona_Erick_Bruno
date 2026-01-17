239de4aab5407be603ad6e9e19a3f81fc9bc29d7
Brunolnf <124039100+Brunolnf@users.noreply.github.com>
2026-01-17 21:22:12 +0100

    Clase Administrador controlando que no pete y con incremento en el id

--- src/main/java/com/example/demo/ProyectoUd5Application.java
+++ src/main/java/com/example/demo/ProyectoUd5Application.java
@@ -3,13 +3,11 @@ package com.example.demo;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
